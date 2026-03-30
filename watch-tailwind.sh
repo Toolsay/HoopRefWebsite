90387091fe127aef2293f0a70d3bc12816b935ba
Edward Brey <edward@breyfamily.net>
2026-03-30 13:34:24 -0500

    Non-consumable in-app purchase. Double tap.

--- watch-tailwind.sh
+++ watch-tailwind.sh
@@ -1,2 +1,3 @@
 # Run while developing to update styles.min.css as new tailwind classes are used.
+npm install --prefer-offline --no-audit --no-fund
 npx @tailwindcss/cli -i styles.css -o styles.min.css --minify --watch
